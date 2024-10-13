7897b56de9ccc212f369c1567f8a97ce574eaac2
jeshwanthshivasai <havingthesensesofaspider@gmail.com>
2024-10-13 21:46:47 +0530

    done!!

--- src/components/BirthdayList.jsx
+++ src/components/BirthdayList.jsx
@@ -1,3 +1,37 @@
+// import React, { useState } from 'react';
+// import birthdays from '../../db';
+
+// const BirthdayList = () => {
+//   const [birthdayList, setBirthdayList] = useState(birthdays);
+//   const clearBirthdays = () => {
+//     setBirthdayList([]);
+//   };
+//   return (
+//     <div>
+//       <h2> 🎂 Birthday List 🎂 </h2>
+
+//       <div className="birthday-List">
+//         {birthdayList.length > 0 ? (
+//           birthdayList.map((person, index) => (
+//             <div key={index} className="person" >
+//               <img src={person.img} alt={`${person.name}'s picture`} />
+//               <h3> {person.name} </h3>
+//               <p> Birthday: {person.birthday} </p>
+//             </div>
+//         ))
+//         ) : (
+//           <p> No birthdays to display! </p>
+//         )} 
+//       </div>
+//       {birthdayList.length > 0 && (
+//         <button onClick={clearBirthdays} className='clear-btn'> Delete all Birthdays </button>
+//       )}
+//     </div>
+//   );
+// }
+// export default BirthdayList;
+
+
 import React, { useState } from 'react';
 import birthdays from '../../db';
 
@@ -10,19 +44,22 @@ const BirthdayList = () => {
     <div>
       <h2> 🎂 Birthday List 🎂 </h2>
 
-      <div className="birthday-List">
-        {birthdayList.length > 0 ? (
-          birthdayList.map((person, index) => (
-            <div key={index} className="person" >
-              <img src={person.img} alt={`${person.name}'s picture`} />
-              <h3> {person.name} </h3>
-              <p> Birthday: {person.birthday} </p>
-            </div>
-        ))
-        ) : (
-          <p> No birthdays to display! </p>
-        )} 
+      <div className="birthday-list-container">
+        <div className="birthday-list">
+          {birthdayList.length > 0 ? (
+            birthdayList.map((person, index) => (
+              <div key={index} className="person">
+                <img src={person.img} alt={`${person.name}'s picture`} />
+                <h3> {person.name} </h3>
+                <p> Birthday: {person.birthday} </p>
+              </div>
+          ))
+          ) : (
+            <p> No birthdays to display! </p>
+          )}
+        </div>
       </div>
+      
       {birthdayList.length > 0 && (
         <button onClick={clearBirthdays} className='clear-btn'> Delete all Birthdays </button>
       )}
@@ -30,3 +67,4 @@ const BirthdayList = () => {
   );
 }
 export default BirthdayList;
+
